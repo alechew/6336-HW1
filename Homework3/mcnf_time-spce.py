d881f1f68f7ae5c859fcae956991de38b654c872
alejandro <alechew@hotmail.com>
2018-02-26 23:41:16 -0500

    adding the inventory arcs

--- Homework3/mcnf_time-spce.py
+++ Homework3/mcnf_time-spce.py
@@ -13,7 +13,7 @@ import MCNF_Objects_hw3
 
 terminalNames = [1, 2, 3, 4, 5, 6]
 
-#  0 is source and 6 is sink
+#  0 is source and 7 is sink
 # name, time, cost
 terminals = {
 	1 : [MCNF_Objects_hw3.City(1, 1, 0), MCNF_Objects_hw3.City(1, 2, 0), MCNF_Objects_hw3.City(1, 3, 0), MCNF_Objects_hw3.City(1, 4, 0), MCNF_Objects_hw3.City(1, 5, 0)],
@@ -21,7 +21,8 @@ terminals = {
 	3 : [MCNF_Objects_hw3.City(3, 1, 0), MCNF_Objects_hw3.City(3, 2, 0), MCNF_Objects_hw3.City(3, 3, 0), MCNF_Objects_hw3.City(3, 4, 0), MCNF_Objects_hw3.City(3, 5, 0)],
 	4 : [MCNF_Objects_hw3.City(4, 1, 0), MCNF_Objects_hw3.City(4, 2, 0), MCNF_Objects_hw3.City(4, 3, 0), MCNF_Objects_hw3.City(4, 4, 0), MCNF_Objects_hw3.City(4, 5, 0)],
 	5 : [MCNF_Objects_hw3.City(5, 1, 0), MCNF_Objects_hw3.City(5, 2, 0), MCNF_Objects_hw3.City(5, 3, 0), MCNF_Objects_hw3.City(5, 4, 0), MCNF_Objects_hw3.City(5, 5, 0)],
-	6 : [MCNF_Objects_hw3.City(6, 1, 0), MCNF_Objects_hw3.City(6, 2, 0), MCNF_Objects_hw3.City(6, 3, 0), MCNF_Objects_hw3.City(6, 4, 0), MCNF_Objects_hw3.City(6, 5, 0)]
+	6 : [MCNF_Objects_hw3.City(6, 1, 0), MCNF_Objects_hw3.City(6, 2, 0), MCNF_Objects_hw3.City(6, 3, 0), MCNF_Objects_hw3.City(6, 4, 0), MCNF_Objects_hw3.City(6, 5, 0)],
+	7 : [MCNF_Objects_hw3.City(7, 6, 0)],
 		}
 
 # creating legs
@@ -41,7 +42,44 @@ legs = [
 	MCNF_Objects_hw3.Leg(5, 6, 4, 1, 0),
 	MCNF_Objects_hw3.Leg(3, 5, 4, 2, 0),
 	MCNF_Objects_hw3.Leg(4, 2, 5, 1, 0),
-	MCNF_Objects_hw3.Leg(4, 5, 5, 2, 0)
+	MCNF_Objects_hw3.Leg(4, 5, 5, 2, 0),
+
+	# inventory arcs
+	MCNF_Objects_hw3.Leg(1, 1, 1, 1, 0),
+	MCNF_Objects_hw3.Leg(1, 1, 2, 1, 0),
+	MCNF_Objects_hw3.Leg(1, 1, 3, 1, 0),
+	MCNF_Objects_hw3.Leg(1, 1, 4, 1, 0),
+	MCNF_Objects_hw3.Leg(1, 1, 5, 1, 0),
+
+	MCNF_Objects_hw3.Leg(2, 2, 1, 1, 0),
+	MCNF_Objects_hw3.Leg(2, 2, 2, 1, 0),
+	MCNF_Objects_hw3.Leg(2, 2, 3, 1, 0),
+	MCNF_Objects_hw3.Leg(2, 2, 4, 1, 0),
+	MCNF_Objects_hw3.Leg(2, 2, 5, 1, 0),
+
+	MCNF_Objects_hw3.Leg(3, 3, 1, 1, 0),
+	MCNF_Objects_hw3.Leg(3, 3, 2, 1, 0),
+	MCNF_Objects_hw3.Leg(3, 3, 3, 1, 0),
+	MCNF_Objects_hw3.Leg(3, 3, 4, 1, 0),
+	MCNF_Objects_hw3.Leg(3, 3, 5, 1, 0),
+
+	MCNF_Objects_hw3.Leg(4, 4, 1, 1, 0),
+	MCNF_Objects_hw3.Leg(4, 4, 2, 1, 0),
+	MCNF_Objects_hw3.Leg(4, 4, 3, 1, 0),
+	MCNF_Objects_hw3.Leg(4, 4, 4, 1, 0),
+	MCNF_Objects_hw3.Leg(4, 4, 5, 1, 0),
+
+	MCNF_Objects_hw3.Leg(5, 5, 1, 1, 0),
+	MCNF_Objects_hw3.Leg(5, 5, 2, 1, 0),
+	MCNF_Objects_hw3.Leg(5, 5, 3, 1, 0),
+	MCNF_Objects_hw3.Leg(5, 5, 4, 1, 0),
+	MCNF_Objects_hw3.Leg(5, 5, 5, 1, 0),
+
+	MCNF_Objects_hw3.Leg(6, 6, 1, 1, 0),
+	MCNF_Objects_hw3.Leg(6, 6, 2, 1, 0),
+	MCNF_Objects_hw3.Leg(6, 6, 3, 1, 0),
+	MCNF_Objects_hw3.Leg(6, 6, 4, 1, 0),
+	MCNF_Objects_hw3.Leg(6, 6, 5, 1, 0),
 ]
 
 # setting legs for each port
@@ -55,6 +93,11 @@ for theTerminal in terminalNames:
 		terminalAndTime = terminal[x]
 		inBoundList = []
 		outBoundList = []
+
+		# for loop to add the legs going to sink only
+		for theLeg in legs:
+			if theLeg.end >= 6:
+				terminals.get(7)[0].inboundLegs.append(theLeg)
 
 		for theLeg in legs:
 			if isinstance(terminalAndTime, MCNF_Objects_hw3.City):
