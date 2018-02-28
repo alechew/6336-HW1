c032e8cc26093e8907a97283c16ee363fca53b47
alejandro <alechew@hotmail.com>
2018-02-28 12:24:23 -0500

    adding lowerbound problem.

---
+++ Homework3/MCNF_Objects_hw3_lowerbound.py
@@ -0,0 +1,21 @@
+class Leg:
+    def __init__(self, org, dest, startTime, daysTraveled, cost, demand):
+        self.origin = org
+        self.start = startTime
+        self.destination = dest
+        self.end = startTime + daysTraveled
+        # if self.end >= 6:
+        #     self.destination = "7"
+        # self.end = 6
+        self.cost = cost
+        self.traveltime = daysTraveled
+        self.arcFlow = 0
+
+
+class City:
+    def __init__(self, name, time, netsupply):
+        self.portName = name
+        self.time = time
+        self.supply = netsupply
+        self.outboundLegs = []
+        self.inboundLegs = []
